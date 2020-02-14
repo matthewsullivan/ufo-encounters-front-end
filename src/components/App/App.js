3654d6517bd1774f462ea4e4479ed6083079619d
Matthew Sullivan <contact@matthewsullivan.media>
2020-02-14 11:37:11 -0500

    Add drawer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,117 @@
 import React from 'react';
 
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+import { withStyles } from '@material-ui/core/styles';
+
 import EncounterMap from '../EncounterMap/EncounterMap';
 
 import './App.css';
 
-export default class App extends React.Component {
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const styles = (theme) => ({
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+});
+
+class App extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
   render() {
-    return <EncounterMap />;
+    const { classes, theme } = this.props;
+    const { open } = this.state;
+
+    return (
+      <>
+        <IconButton
+          aria-label="Open drawer"
+          className={classNames(classes.menuButton)}
+          color="inherit"
+          onClick={this.handleDrawerOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          anchor="right"
+          className={classes.drawer}
+          variant="persistent"
+          open={open}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.drawerHeader}>
+            <IconButton onClick={this.handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+
+        <EncounterMap />;
+      </>
+    );
   }
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(App);
